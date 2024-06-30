e4ec83535759de9eeb47d44caabbaf87e9676aa3
alami khalil <alamikhalil@hotmail.fr>
2024-06-30 19:06:01 +0200

    Update bin.install to use correct executable name

--- qoarks_cli.rb
+++ qoarks_cli.rb
@@ -6,7 +6,7 @@ class QoarksCli < Formula
     sha256 "b8188e6fd5180b2d6c72ab1be0adda287b66199d7585da677b9f178d5c625d9d"
   
     def install
-      bin.install "qoarks_cli"
+      bin.install "qoarks_cli-v0.13.0-darwin-amd64" => "qoarks_cli"
     end
   end
   
